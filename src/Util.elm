8535411bbee10c683fccc8db53eae6b93ea585a8
Baumann Manuel <mbaumann@gmail.com>
2016-06-15 00:55:25 +0200

    switch to svg
    no luck with foreign object :(

--- src/Util.elm
+++ src/Util.elm
@@ -11,22 +11,19 @@ vecToSvgPos : Vec2 -> String
 vecToSvgPos vec = 
     (toString <| getX vec) ++ " " ++ (toString <| getY vec) 
 
-
-
 warningsFor : String -> String
 warningsFor moduleFileName =
     "elm-make --warn " ++ moduleFileName
 
 getAt: Int->Array number->number    
 getAt idx arr = Array.get idx arr |> Maybe.withDefault 0
+
 setAt: Int->number->Array number->Array number    
 setAt idx val arr = Array.set idx val arr 
-
 
 updateMany : List a -> (a->b->(b, Cmd a))-> ( b, Cmd a ) -> ( b, Cmd a )
 updateMany msgs update modelCmd =
     List.foldl (updateOne update) modelCmd msgs
-
 
 updateOne : (a->b->(b, Cmd a))-> a -> ( b, Cmd a ) -> ( b, Cmd a )
 updateOne update msg ( model, effect ) =
@@ -44,6 +41,9 @@ fromIntTuple ( x, y ) = fromIntRecord { x = x, y = y }
 
 toIntRecord : Vec2 ->  { x : Int, y : Int }
 toIntRecord v = {  x = round <| getX v, y = round <| getY v }
+
+toIntTuple : Vec2 ->  (Int,Int)
+toIntTuple v = ( round <| getX v, round <| getY v )
 
 multiplyVec: Vec2 -> Vec2 -> Vec2
 multiplyVec v1 v2 = 
